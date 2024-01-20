70ee592dda80f097bc87306d83880dda52ed06ec
Brian-Machiestay <brianmachiestay@gmail.com>
2024-01-20 12:27:49 +0000

    complete sidebar styling

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ import Mainpage from './components/main/mainpage';
 
 function App() {
   return (
-    <>
+    <div className='home'>
       <Sidebar />
       <Mainpage />
-    </>
+    </div>
   );
 }
 
--- src/components/sidebar/sidebar.jsx
+++ src/components/sidebar/sidebar.jsx
@@ -4,7 +4,10 @@ import styles from './sidebar.module.css'
 const Sidebar = () => {
     return(
         <div className={styles.container}>
-            <p>Now the sidebar</p>
+            <p className={styles.logo}>TeachMateAI</p>
+            <p className={styles.active}>Tasks</p>
+            <p>Notification</p>
+            <p>Notification</p>
         </div>
     )
 }
