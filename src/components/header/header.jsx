d3f8a5f7352b753f3d9ab29e37460ba39a61d9ae
Brian-Machiestay <brianmachiestay@gmail.com>
2024-01-20 14:39:27 +0000

    design header component responsively

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import styles from "./header.module.css";
+import menu from "../../assets/menu.svg";
+import search from "../../assets/search.svg";
 
 
 const Header = () => {
     return (
         <div className={styles.container}>
-            <p>This is the header</p>
+            <img src={menu} alt="menu" className={styles.menu} />
+            <p>Tasks</p>
+            <div className={styles.search}>
+                <img src={search} alt="search" className={styles.searchIcon} />
+                <input type="text" />
+            </div>
+            <button className={styles.addColumn}>+</button>
         </div>
     )
 }
